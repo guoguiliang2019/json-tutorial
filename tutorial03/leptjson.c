82519bdb34dc367f2a03f8386ceca2f17a9f65d5
guoguiliang2019 <1430912397@qq.com>
2022-07-14 17:51:59 +0800

    已完成第3单元练习

--- tutorial03/leptjson.c
+++ tutorial03/leptjson.c
@@ -16,10 +16,18 @@
 
 typedef struct {
     const char* json;
+    /*堆栈缓冲区和栈大小、栈顶位置*/
     char* stack;
     size_t size, top;
 }lept_context;
 
+/**
+ * @brief  
+ * 
+ * @param c 
+ * @param size 
+ * @return void* 
+ */
 static void* lept_context_push(lept_context* c, size_t size) {
     void* ret;
     assert(size > 0);
@@ -35,6 +43,13 @@ static void* lept_context_push(lept_context* c, size_t size) {
     return ret;
 }
 
+/**
+ * @brief 出栈,设置栈顶位置，返回栈顶指针
+ * 
+ * @param c 
+ * @param size 
+ * @return void* 
+ */
 static void* lept_context_pop(lept_context* c, size_t size) {
     assert(c->top >= size);
     return c->stack + (c->top -= size);
@@ -47,6 +62,15 @@ static void lept_parse_whitespace(lept_context* c) {
     c->json = p;
 }
 
+/**
+ * @brief 解析文本量
+ * 
+ * @param c 
+ * @param v 
+ * @param literal 
+ * @param type 
+ * @return int 
+ */
 static int lept_parse_literal(lept_context* c, lept_value* v, const char* literal, lept_type type) {
     size_t i;
     EXPECT(c, literal[0]);
@@ -58,6 +82,13 @@ static int lept_parse_literal(lept_context* c, lept_value* v, const char* litera
     return LEPT_PARSE_OK;
 }
 
+/**
+ * @brief 解析数值
+ * 
+ * @param c 
+ * @param v 
+ * @return int 
+ */
 static int lept_parse_number(lept_context* c, lept_value* v) {
     const char* p = c->json;
     if (*p == '-') p++;
@@ -86,6 +117,13 @@ static int lept_parse_number(lept_context* c, lept_value* v) {
     return LEPT_PARSE_OK;
 }
 
+/**
+ * @brief 解析字符串
+ * 
+ * @param c 
+ * @param v 
+ * @return int 
+ */
 static int lept_parse_string(lept_context* c, lept_value* v) {
     size_t head = c->top, len;
     const char* p;
@@ -102,23 +140,67 @@ static int lept_parse_string(lept_context* c, lept_value* v) {
             case '\0':
                 c->top = head;
                 return LEPT_PARSE_MISS_QUOTATION_MARK;
+            case '\\':
+                /*其后方是一个转义序列*/
+            {
+                ch = *p++;
+                switch(ch)
+                {
+                    case '\\':
+                        PUTC(c, '\\');break;
+                    case '\"':
+                        PUTC(c, '\"');break;
+                    case '/':
+                        PUTC(c, '/');break;
+                    case 'b':
+                        PUTC(c, '\b');break;
+                    case 'f':
+                        PUTC(c, '\f');break;
+                    case 'n':
+                        PUTC(c, '\n');break;
+                    case 'r':
+                        PUTC(c, '\r');break;
+                    case 't':
+                        PUTC(c, '\t');break;
+                    default:
+                        c->top = head;
+                        return LEPT_PARSE_INVALID_STRING_ESCAPE;
+                }
+                break;
+            }
             default:
+                if((unsigned char)ch < 0x20)
+                {
+                    c->top = head;
+                    return LEPT_PARSE_INVALID_STRING_CHAR;
+                }
                 PUTC(c, ch);
         }
     }
 }
 
+/**
+ * @brief 解析器的状态机
+ * 
+ * @param c 
+ * @param v 
+ * @return int 
+ */
 static int lept_parse_value(lept_context* c, lept_value* v) {
     switch (*c->json) {
         case 't':  return lept_parse_literal(c, v, "true", LEPT_TRUE);
         case 'f':  return lept_parse_literal(c, v, "false", LEPT_FALSE);
         case 'n':  return lept_parse_literal(c, v, "null", LEPT_NULL);
-        default:   return lept_parse_number(c, v);
         case '"':  return lept_parse_string(c, v);
         case '\0': return LEPT_PARSE_EXPECT_VALUE;
+        default:   return lept_parse_number(c, v);
     }
 }
 
+/**
+ * @brief 解析器API
+ * 
+ */
 int lept_parse(lept_value* v, const char* json) {
     lept_context c;
     int ret;
@@ -140,6 +222,11 @@ int lept_parse(lept_value* v, const char* json) {
     return ret;
 }
 
+/**
+ * @brief 释放json值空间
+ * 
+ * @param v 
+ */
 void lept_free(lept_value* v) {
     assert(v != NULL);
     if (v->type == LEPT_STRING)
@@ -147,39 +234,97 @@ void lept_free(lept_value* v) {
     v->type = LEPT_NULL;
 }
 
+/**
+ * @brief 获取json值类型
+ * 
+ * @param v 
+ * @return lept_type 
+ */
 lept_type lept_get_type(const lept_value* v) {
     assert(v != NULL);
     return v->type;
 }
 
+/**
+ * @brief 获取bool类型的json值
+ * 
+ * @param v 
+ * @return int 
+ */
 int lept_get_boolean(const lept_value* v) {
     /* \TODO */
-    return 0;
+    assert(v != NULL && (v->type == LEPT_FALSE || v->type == LEPT_TRUE));
+    return v->type == LEPT_TRUE;
 }
 
+/**
+ * @brief 设置bool类型的json值
+ * 
+ * @param v 
+ * @param b 
+ */
 void lept_set_boolean(lept_value* v, int b) {
     /* \TODO */
+    lept_free(v);
+    if(b == 0)
+        v->type = LEPT_FALSE;
+    else
+        v->type = LEPT_TRUE;
 }
 
+/**
+ * @brief 获取number类型的json值
+ * 
+ * @param v 
+ * @return double 
+ */
 double lept_get_number(const lept_value* v) {
     assert(v != NULL && v->type == LEPT_NUMBER);
     return v->u.n;
 }
 
+/**
+ * @brief 设置number类型的json值
+ * 
+ * @param v 
+ * @param n 
+ */
 void lept_set_number(lept_value* v, double n) {
     /* \TODO */
+    lept_free(v);
+    v->u.n = n;
+    v->type = LEPT_NUMBER;
 }
 
+/**
+ * @brief 获得string类型的json值
+ * 
+ * @param v 
+ * @return const char* 
+ */
 const char* lept_get_string(const lept_value* v) {
     assert(v != NULL && v->type == LEPT_STRING);
     return v->u.s.s;
 }
 
+/**
+ * @brief 获取string类型json值的长度
+ * 
+ * @param v 
+ * @return size_t 
+ */
 size_t lept_get_string_length(const lept_value* v) {
     assert(v != NULL && v->type == LEPT_STRING);
     return v->u.s.len;
 }
 
+/**
+ * @brief 设置string类型的json值
+ * 
+ * @param v 
+ * @param s 
+ * @param len 
+ */
 void lept_set_string(lept_value* v, const char* s, size_t len) {
     assert(v != NULL && (s != NULL || len == 0));
     lept_free(v);
--- tutorial03/leptjson.h
+++ tutorial03/leptjson.h
@@ -3,8 +3,10 @@
 
 #include <stddef.h> /* size_t */
 
+
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
+/*json值的数据结构定义*/
 typedef struct {
     union {
         struct { char* s; size_t len; }s;  /* string: null-terminated string, string length */
@@ -13,6 +15,7 @@ typedef struct {
     lept_type type;
 }lept_value;
 
+/*解析函数的返回值*/
 enum {
     LEPT_PARSE_OK = 0,
     LEPT_PARSE_EXPECT_VALUE,
@@ -24,22 +27,29 @@ enum {
     LEPT_PARSE_INVALID_STRING_CHAR
 };
 
+/*初始化json值*/
 #define lept_init(v) do { (v)->type = LEPT_NULL; } while(0)
 
 int lept_parse(lept_value* v, const char* json);
 
+/*释放json值空间*/
 void lept_free(lept_value* v);
 
+/*json值类型的读取函数*/
 lept_type lept_get_type(const lept_value* v);
 
+/*null值的设置函数*/
 #define lept_set_null(v) lept_free(v)
 
+/*bool值的读写函数*/
 int lept_get_boolean(const lept_value* v);
 void lept_set_boolean(lept_value* v, int b);
 
+/*数值的读写函数*/
 double lept_get_number(const lept_value* v);
 void lept_set_number(lept_value* v, double n);
 
+/*string值和string_length的读写函数*/
 const char* lept_get_string(const lept_value* v);
 size_t lept_get_string_length(const lept_value* v);
 void lept_set_string(lept_value* v, const char* s, size_t len);
